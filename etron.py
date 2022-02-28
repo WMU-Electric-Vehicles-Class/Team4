143290590defd115bb3ad8e7a5d801db9216403d
Kyle Carow <kylecarow@gmail.com>
2022-02-28 08:41:34 -0500

    Reorganized repo

--- etron.py
+++ etron.py
@@ -10,25 +10,58 @@ class e_tron:
 
             g = 9.81
 
-            self.curb_mass = 2490  # kg
-            self.weight = self.curb_mass * g  # N
+            self.mass = 2595  # kg
+            self.weight = self.mass * g  # N
 
-            self.parent.grade = 0  # rise/run
-            self.parent.road_angle = np.arctan(self.parent.grade)  # radians
+            # Aerodynamics
+            self.rho = 1.225  # Density of air at STP, kg/m^3
+            self.Cd = 0.25  # Coefficient of drag
+            self.A = 2.65  # Frontal area, m^2 https://www.auk-kits.co.uk/assets/documents/original/8386-14AudietronAerodynamics.pdf
+
+            # Rolling resistance, Frr = µrr * W
+            self.mu_rr = 0.015
+            self.F_rr = self.mu_rr * self.weight
 
         def step(self):
-            pass
+            road_angle = np.arctan(self.parent.grade)  # radians
+            self.F_grade = self.weight * np.sin(road_angle)  # in negative x direction
+
+            self.F_drag = (
+                1 / 2 * self.rho * self.parent.velocity**2 * self.Cd * self.A
+            )  # in negative x direction
+
+            # Fx - Fdrag - Frr - Fgrade = m*a
+            Fx = (
+                self.mass * self.parent.acceleration
+                + self.F_drag
+                + self.F_rr
+                + self.F_grade
+            )
+
+            if Fx >= 0:
+                # Driving
+                self.F_drive = Fx
+                self.F_brake = 0
+            elif Fx < 0:
+                # Braking
+                self.F_drive = 0
+                self.F_brake = -Fx
 
     class Wheel:
         def __init__(self, parent):
             self.parent = parent
 
             INCH_TO_M = 0.0254
-
-            tire_radius_in = 15.02  # inches  https://www.wheel-size.com/size/audi/e-tron/2020/  https://www.1010tires.com/Tools/Tire-Size-Calculator/255-55R19
+            tire_radius_in = (
+                0.98 * 15.02
+            )  # in  https://www.wheel-size.com/size/audi/e-tron/2020/  https://www.1010tires.com/Tools/Tire-Size-Calculator/255-55R19
             self.tire_radius = tire_radius_in * INCH_TO_M  # meters
 
         def step(self):
+            # Driving torque TODO: add regenerative braking
+            driving_torque = self.parent.chassis.F_drive * self.tire_radius
+            self.torque_front = 0.40 * driving_torque
+            self.torque_rear = 0.60 * driving_torque
             self.angular_velocity = self.parent.velocity / self.tire_radius
 
     class Gearbox:
@@ -38,15 +71,20 @@ class e_tron:
             # https://electrichasgoneaudi.net/models/e-tron/drivetrain/motor/
             # https://static.nhtsa.gov/odi/tsbs/2019/MC-10155750-9999.pdf
             # Front gearbox torque ratio
-            self.front_ratio = 9.205
+            self.ratio_front = 9.205
             # Rear gearbox torque ratio
-            self.rear_ratio = 9.083
+            self.ratio_rear = 9.083
 
         def step(self):
             wheel_angular_velocity = self.parent.wheel.angular_velocity
+            wheel_torque_front = self.parent.wheel.torque_front
+            wheel_torque_rear = self.parent.wheel.torque_rear
 
-            self.front_motor_speed = wheel_angular_velocity * self.front_ratio
-            self.rear_motor_speed = wheel_angular_velocity * self.rear_ratio
+            self.motor_torque_front = wheel_torque_front / self.ratio_front
+            self.motor_speed_front = wheel_angular_velocity * self.ratio_front
+
+            self.motor_torque_rear = wheel_torque_rear / self.ratio_rear
+            self.motor_speed_rear = wheel_angular_velocity * self.ratio_rear
 
     class Motor:
         def __init__(self, parent):
@@ -144,25 +182,44 @@ class e_tron:
                 raise Exception("Motor velocity out of bounds")
 
         def step(self):
-            self.speed_front = self.parent.gearbox.front_motor_speed
-            self.speed_rear = self.parent.gearbox.rear_motor_speed
+            # Front motor torque and speed
+            self.torque_front = self.parent.gearbox.motor_torque_front
+            self.speed_front = self.parent.gearbox.motor_speed_front
+            self.rpm_front = self.speed_front * 60 / (2 * np.pi)
 
-            # Calculate RPM
-            self.nmr_front = self.speed_front * 60 / (2 * np.pi)
-            self.nmr_rear = self.speed_rear * 60 / (2 * np.pi)
+            # Rear motor torque and speed
+            self.torque_rear = self.parent.gearbox.motor_torque_rear
+            self.speed_rear = self.parent.gearbox.motor_speed_rear
+            self.rpm_rear = self.speed_rear * 60 / (2 * np.pi)
 
-            # Calculate output torque
-            self.Tm_front = self.front_torque_Nm(self.nmr_front)
-            self.Tm_rear = self.rear_torque_Nm(self.nmr_rear)
+            if not self.rpm_front == 0:
+                # Calculate max output torque front
+                self.torque_front_max = self.front_torque_Nm(self.rpm_front)
+                self.control_signal_front = self.torque_front / self.torque_front_max
+
+                # Calculate max output torque rear
+                self.torque_rear_max = self.rear_torque_Nm(self.rpm_rear)
+                self.control_signal_rear = self.torque_rear / self.torque_rear_max
+            else:
+                self.control_signal_front = 0
+                self.control_signal_rear = 0
 
             # Calculate output power
-            self.Pout_front = self.front_power_kW(self.nmr_front)
-            self.Pout_rear = self.rear_power_kW(self.nmr_rear)
+            self.power_out_front = self.torque_front * self.speed_front
+            self.power_out_rear = self.torque_rear * self.speed_rear
+            a.append(self.power_out_front)
+            b.append(self.power_out_rear)
+
+            # Calculate input power
+            eta_front = 1.00  # TODO: replace with efficiency map function
+            eta_rear = 1.00  # TODO: replace with efficiency map function
+            self.power_in_front = self.power_out_front / eta_front
+            self.power_in_rear = self.power_out_rear / eta_rear
 
             # Calculate supply frequencies
             # nmr = 120 * fs / p
-            self.fs_front = self.p / 120 * self.nmr_front  # Front supply frequency, Hz
-            self.fs_rear = self.p / 120 * self.nmr_rear
+            self.fs_front = self.p / 120 * self.rpm_front  # Front supply frequency, Hz
+            self.fs_rear = self.p / 120 * self.rpm_rear
 
     class Battery:
         def __init__(self, parent, SOC_initial):
@@ -197,15 +254,16 @@ class e_tron:
         cycle["Velocity (m/s)"] = cycle["Velocity (mph)"] * MPH_TO_MPS
 
         for i in range(len(self.cycle)):
-            self.time_s = self.cycle["Time (s)"].iloc[i]
+            self.time = self.cycle["Time (s)"].iloc[i]
             self.velocity = self.cycle["Velocity (m/s)"].iloc[i]
+            self.grade = 0  # TODO: replace
 
             # Vehicle dynamics calculations
             if i > 0:
-                self.prev_time_s = self.cycle["Time (s)"].iloc[i - 1]
+                self.prev_time = self.cycle["Time (s)"].iloc[i - 1]
                 self.prev_velocity = self.cycle["Velocity (m/s)"].iloc[i - 1]
 
-                self.dt = self.time_s - self.prev_time_s
+                self.dt = self.time - self.prev_time
                 self.dv = self.velocity - self.prev_velocity
 
                 # Change in distance
@@ -231,17 +289,28 @@ class e_tron:
         pass
 
 
-"""a = []
-b = []"""
+if __name__ == "__main__":
+    a = []
+    b = []
 
-my_fancy_ev = e_tron()
+    my_fancy_ev = e_tron()
 
-hwfet = pd.read_csv("hwycol.txt", sep="\t")
+    hwfet = pd.read_csv("data/hwycol.txt", sep="\t")
 
-my_fancy_ev.simulate(hwfet)
+    my_fancy_ev.simulate(hwfet)
 
-"""from matplotlib import pyplot as plt
+    from matplotlib import pyplot as plt
+    import seaborn as sns
 
-plt.plot(list(range(len(a))), a)
-plt.plot(list(range(len(b))), b)
-plt.show()"""
+    sns.set()
+    a = np.array(a)
+    b = np.array(b)
+
+    plt.gcf().set_size_inches(3.5 * 3, 1.5 * 3)
+    plt.plot(list(range(len(a))), a / 1000)
+    plt.plot(list(range(len(b))), b / 1000)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Power [kW]")
+    plt.legend(["Front motor power output", "Rear motor power output"])
+    plt.tight_layout()
+    plt.savefig("plot.png")
